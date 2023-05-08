8338786746628d390a82198dc1d6f01b1301be32
Hokage09das <dbayizbekov96@gmail.com>
2023-05-08 17:07:36 +0600

    inin about page

--- src/pages/about-page/AboutPage.jsx
+++ src/pages/about-page/AboutPage.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import { AvenueCarousel, Layout } from '../../components';
+
+import { HomeAboutSection } from '../../widgets/home_about_section/HomeAboutSection';
+import { WelcomingSection } from '../../widgets/welcoming_section/WelcomingSection';
+import { AboutSecondSection } from '../../widgets/about_second_section/AboutSecondSection';
+
 export const AboutPage = () => {
-	return <div>AboutPage</div>;
+	return (
+		<div>
+			<Layout>
+				<HomeAboutSection />
+				<WelcomingSection />
+				<AvenueCarousel />
+				<AboutSecondSection />
+			</Layout>
+		</div>
+	);
 };
